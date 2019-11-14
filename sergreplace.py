ce4f554cea4f65e89f0645c9ad8082c73f3206e3
Tymoteusz Brzozowski <35313475+Paz5@users.noreply.github.com>
2019-11-14 16:52:45 +0100

    Added script

---
+++ sergreplace.py
@@ -0,0 +1,20 @@
+f = open("sergascii.txt")
+string = ''
+i = 0
+CHARTOREPLACE = ','
+STRINGREPLACEMENT = 'sphinx of black quartz, judge my vow'
+
+for line in f:
+	for char in line:
+		if char == CHARTOREPLACE:
+			if i > (len(STRINGREPLACEMENT)-1):
+				i = 0
+			string += STRINGREPLACEMENT[i]
+			i+=1
+		else:
+			string += char
+
+f.close()
+out = open(f'replaced_with_{STRINGREPLACEMENT}.txt','w')
+out.write(string)
+out.close()
